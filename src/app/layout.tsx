10aa32bbf2ced356cb66093331f0d33a9e9dc8a0
Shubham Nagar <shubhamnagar234@gmail.com>
2026-05-19 19:57:10 +0530

    Header and Navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Source_Sans_3 as FontSans } from 'next/font/google';
 import './globals.css';
+import Header from '@/components/common/header';
+import Footer from '@/components/common/footer';
 
 const fontSans = FontSans({
   variable: '--font-sans',
@@ -20,8 +22,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${fontSans.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en">
+      <body className={`${fontSans.variable} antialiased`}>
+        <div className="relative flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-1">{children}</main>
+          <Footer />
+        </div>
+      </body>
     </html>
   );
 }
