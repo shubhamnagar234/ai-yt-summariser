cf5693efb919205094e84c9e681f465c30114ca6
Shubham Nagar <shubhamnagar234@gmail.com>
2026-05-19 21:38:30 +0530

    How It Works and CTA Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import BgGradient from '@/components/common/bg-gradient';
 import HeroSection from '@/components/home/hero-section';
 import DemoSection from '@/components/home/demo-section';
+import HowItWorksSection from '@/components/home/how-it-works-section';
+import CTASection from '@/components/home/cta-section';
 
 export default function Home() {
   return (
@@ -9,6 +11,8 @@ export default function Home() {
       <div className="flex flex-col">
         <HeroSection />
         <DemoSection />
+        <HowItWorksSection />
+        <CTASection />
       </div>
     </div>
   );
