ee4c0046f4411c33bbaffdc02257a74e5588cb75
Shubham Nagar <shubhamnagar234@gmail.com>
2026-05-20 00:12:56 +0530

    Clerk Authentication

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,9 +1,8 @@
 import { SquarePlay } from 'lucide-react';
-import { Button } from '../ui/button';
 import NavLink from './nav-link';
+import { SignInButton, SignUpButton, UserButton } from '@clerk/nextjs';
 
 export default function Header() {
-  const isLoggedIn = false;
   return (
     <nav className="container flex items-center justify-between py-4 lg:px-8 px-2 mx-auto">
       <div className="flex lg:flex-1">
@@ -16,18 +15,23 @@ export default function Header() {
       </div>
       <div className="flex lg:justify-center gap-4 lg:gap-12 lg:items-center">
         <NavLink href="/#pricing">Pricing</NavLink>
-        {isLoggedIn && <NavLink href="/#dashboard">Your Summaries</NavLink>}
+        <SignInButton>
+          <NavLink href="/#dashboard">Your Summarize</NavLink>
+        </SignInButton>
       </div>
       <div className="flex lg:justify-end lg:flex-1">
-        {isLoggedIn ? (
+        <SignInButton>
           <div className="flex gap-2 items-center">
-            <NavLink href="/upload">Upload a PDF</NavLink>
-            <div>Pro</div>
-            <Button>User</Button>
+            {/* <NavLink href="/upload">Upload a PDF</NavLink>
+            <div>Pro</div> */}
+            <SignInButton>
+              <UserButton />
+            </SignInButton>
           </div>
-        ) : (
+        </SignInButton>
+        <SignUpButton>
           <NavLink href="/sign-in">Sign In</NavLink>
-        )}
+        </SignUpButton>
       </div>
     </nav>
   );
